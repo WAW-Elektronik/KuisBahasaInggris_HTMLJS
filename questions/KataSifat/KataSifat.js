bb90d2e9d41ee6ecd8092119c52a862d3d7eb570
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-06-08 06:44:55 +0700

    Add files via upload

---
+++ questions/KataSifat/KataSifat.js
@@ -0,0 +1,7 @@
+window.loadedQuestions = [
+  { word: "beautiful", arti: "cantik" },
+  { word: "strong", arti: "kuat" },
+  { word: "happy", arti: "bahagia" },
+  { word: "angry", arti: "marah" },
+  { word: "brave", arti: "berani" }
+];
