a3ac3a509aa6a87659500ededa00fb26faac6f75
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-06-23 16:56:08 +0700

    Add files via upload

---
+++ questions/OrdinalNumber/OrdinalNumberPenggunaan.js
@@ -0,0 +1,8 @@
+window.loadedQuestions = [
+    { Soal: "Hari ini tanggal 21 Juni. <br> Today is the ... of June.", Jawab: "21st" },
+    { Soal: "Ulang tahunnya tanggal tiga Mei. <br> Her birthday is on the ... of May.", Jawab: "Third" },
+    { Soal: "Dia juara pertama dalam perlombaan. <br> She came ... in the race.", Jawab: "First" },
+    { Soal: "Ini percobaan kedua saya. <br> This is my ... attempt.", Jawab: "Second" },
+    { Soal: "Saya tinggal di lantai lima. <br> I live on the ... floor.", Jawab: "Fifth" },
+    { Soal: "Baca bab ketiga dengan saksama. <br> Read the third chapter carefully.", Jawab: "Third"}
+];
